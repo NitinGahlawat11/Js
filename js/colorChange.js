626fcfe4b1cc4a2c3cf1cdb111d8639aebd70935
NitinGahlawat11 <37336987+NitinGahlawat11@users.noreply.github.com>
2018-08-02 19:10:25 +0530

    Random backgrounnd generator

---
+++ js/colorChange.js
@@ -0,0 +1,19 @@
+// window.alert("js connected");
+// window.onload=function (ev) {
+//
+//     var main = document.getElementById('main-new');
+//     var btn = document.getElementById('changeColor');
+//
+//     document.getElementById('changeColor').onclick = function () {
+//         var x = Math.round(Math.random() * 256);
+//         var y = Math.round(Math.random() * 256);
+//         var z = Math.round(Math.random() * 256);
+//         var color = 'rgb(' + x + ',' + y + ',' + z + ')';
+//         main.style.backgroundColor = color;
+//     }
+// }
+function greet(name) {
+console.log('hello ' + name );
+}
+ var nitin ="nits"
+greet(nitin);
